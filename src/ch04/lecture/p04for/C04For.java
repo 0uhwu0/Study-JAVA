8e940a3d3063ff19c15c84bf3a56e12b6a7f3ed7
0uhwu0 <dbgusdn2048@naver.com>
2023-02-21 17:48:23 +0900

    2023-02-21

--- src/ch04/lecture/p04for/C04For.java
+++ src/ch04/lecture/p04for/C04For.java
@@ -31,4 +31,15 @@ public class C04For {
 			System.out.println();
 		}
 	}
+	class Solution {
+	    public int[] solution(int[] numbers, int num1, int num2) {
+	        int[] answer = {};
+	        int a = 0;
+	        for(int i = num1; i <= num2; i++) {
+	            answer[a] = numbers[i];
+	            a++;
+	        }
+	        return answer;
+	    }
+	}
 }
