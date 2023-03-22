4b185407c6c701afa6975717f7c5e40de10cd950
0uhwu0 <dbgusdn2048@naver.com>
2023-03-22 17:48:33 +0900

    2023-03-22

---
+++ src/test/programmers/S120848.java
@@ -0,0 +1,15 @@
+package test.programmers;
+
+public class S120848 {
+	public static void main(String[] args) {
+		int n = 3628800;
+		int answer = 1;
+		int result = 0;
+		for(int i = 1; i <= 10; i++) {
+			answer *= i;
+			result = i;
+		}
+		
+		System.out.println(result);asd
+		}
+}
