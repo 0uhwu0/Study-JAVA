77e199bc58ff8e538ffed636a6de673496e12f6e
0uhwu0 <dbgusdn2048@naver.com>
2023-03-23 17:15:02 +0900

    2023-03-23

--- src/test/programmers/S120848.java
+++ src/test/programmers/S120848.java
@@ -3,13 +3,15 @@ package test.programmers;
 public class S120848 {
 	public static void main(String[] args) {
 		int n = 3628800;
-		int answer = 1;
-		int result = 0;
-		for(int i = 1; i <= 10; i++) {
-			answer *= i;
-			result = i;
-		}
-		
-		System.out.println(result);asd
-		}
+		int fac = 1;
+        int i = 0;
+
+        while(true){
+            if(fac <= n){
+                fac *= i + 1;
+                i++;
+            }else break;
+        }
+        System.out.println(i-1);
+    }
 }
