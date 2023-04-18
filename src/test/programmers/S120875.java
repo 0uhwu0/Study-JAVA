8a97859b090a760432b01a565e1b6c5d54f9d7d1
0uhwu0 <dbgusdn2048@naver.com>
2023-04-18 17:49:03 +0900

    2023-04-18

---
+++ src/test/programmers/S120875.java
@@ -0,0 +1,7 @@
+package test.programmers;
+
+public class S120875 {
+	public static void main(String[] args) {
+		int[][] dots = asd
+	}
+}
--- src/test/programmers/S120913.java
+++ src/test/programmers/S120913.java
@@ -7,18 +7,14 @@ public class S120913 {
 		String my_str = "abc1Addfggg4556b";
 		int n = 6;
 		String[] answer;
-		int a = 0;
-		String new_str = "";
 		if (my_str.length() % n == 0) {
 			answer = new String[my_str.length()/n];
 		} else {
 			answer = new String[my_str.length()/n + 1];
 		}
 		
-		for (int i = n; i <= my_str.length(); i+=n) {asd
-			answer[a] = my_str.substring(0, i);
-			System.out.println(Arrays.toString(answer));
-			a++;
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = (i * n + n <= my_str.length() ? my_str.substring(i * n, i * n + n) : my_str.substring(i * n));
 		}
 		System.out.println(Arrays.toString(answer));
 	}
