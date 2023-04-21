2c1378b835e2649e4fa8b28935fad6157873205e
0uhwu0 <dbgusdn2048@naver.com>
2023-04-21 17:47:41 +0900

    2023-04-21

--- src/test/programmers/S120875.java
+++ src/test/programmers/S120875.java
@@ -6,7 +6,7 @@ package test.programmers;
 // 03 12
 public class S120875 {
 	public static void main(String[] args) {
-		int[][] dots = {{1, 4}, {9, 2}, {3, 8}, {11, 6}};
+		int[][] dots = {{3, 5}, {4, 1}, {2, 4}, {5, 10}};
 		int answer = 0;
 		int x0 = dots[0][0];
 		int y0 = dots[0][1];
@@ -20,11 +20,27 @@ public class S120875 {
 		int x3 = dots[3][0];
 		int y3 = dots[3][1];
 		
-		if (((y0-y1) / (x0-x1)) == ((y2-y3) / (x2-x3))) {
+		double a1 = ((y0-y1) / (x0-x1)) + ((y0-y1) % (x0-x1));
+		double a2 = ((y2-y3) / (x2-x3)) + ((y2-y3) % (x2-x3));
+		
+		if (a1 == a2) {
 			answer = 1;
-		} else {
-			answer = 0;
-		}asdaisdj
+		}
+		
+		double b1 = ((y0-y2) / (x0-x2)) + ((y0-y2) % (x0-x2));
+		double b2 = ((y1-y3) / (x1-x3)) + ((y1-y3) % (x1-x3));
+		
+		if (b1 == b2) {
+			answer = 1;
+		}
+		
+		double c1 = ((y0-y3) / (x0-x3)) + ((y0-y3) % (x0-x3));
+		double c2 = ((y1-y2) / (x1-x2)) + ((y1-y2) % (x1-x2));
+		
+		if (c1 == c2) {
+			answer = 1;
+		}
+		
 		System.out.println(answer);
 	}
 }
---
+++ src/test/programmers/S120876.java
@@ -0,0 +1,14 @@
+package test.programmers;
+
+public class S120876 {
+	public static void main(String[] args) {
+		int[][] lines = {{0, 1}, {2, 5}, {3, 9}}; // x좌표 0~1, 2~5, 3~9
+		// lines의 각 인덱스의 [][0]의 제일 작은 값 찾고
+		// 
+		int answer = 0;
+		
+		
+		
+		System.out.println(answer);
+	}
+}
