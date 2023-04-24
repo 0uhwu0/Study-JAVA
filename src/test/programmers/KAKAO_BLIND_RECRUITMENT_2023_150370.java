0d3e04202ad2f24cdce6b7c511abcd312052d256
0uhwu0 <dbgusdn2048@naver.com>
2023-04-24 17:45:14 +0900

    2023-04-24

---
+++ src/test/programmers/KAKAO_BLIND_RECRUITMENT_2023_150370.java
@@ -0,0 +1,13 @@
+package test.programmers;
+
+public class KAKAO_BLIND_RECRUITMENT_2023_150370 {
+	public static void main(String[] args) {
+		// 매 달이 28일까지라고 가정
+		String today = "2022.05.19";
+		String[] terms = {"A 6", "B 12", "C 3"};
+		String[] privacies = {"2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"};
+		
+		
+	}
+}
+asdasd
---
+++ src/test/programmers/S176963.java
@@ -0,0 +1,13 @@
+package test.programmers;
+
+public class S176963 {
+	public static void main(String[] args) {
+		String[] name = {"may", "kein", "kain", "radi"};
+		int[] yearning = {5, 10, 1, 3};
+		String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+		int sum = 0;
+		for (int i = 0; i < yearning.length; i++) {
+			sum += yearning[i];
+		}
+	}
+}
