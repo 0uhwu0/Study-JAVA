424d9e9d4a28e3a13f97fe6d9b15254d3687d730
0uhwu0 <dbgusdn2048@naver.com>
2023-04-25 17:48:05 +0900

    2023-04-25

--- src/test/programmers/S176963.java
+++ src/test/programmers/S176963.java
@@ -5,9 +5,15 @@ public class S176963 {
 		String[] name = {"may", "kein", "kain", "radi"};
 		int[] yearning = {5, 10, 1, 3};
 		String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+		int answer[] = new int[photo.length];
+		System.out.println(photo.length);
 		int sum = 0;
 		for (int i = 0; i < yearning.length; i++) {
 			sum += yearning[i];
+		}
+		
+		for (int i = 0; i < photo.length; i++) {
+			
 		}
 	}
 }
