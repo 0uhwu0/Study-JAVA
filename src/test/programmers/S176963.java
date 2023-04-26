bf2f795400e039ed24a64271879ef4cd37f95346
0uhwu0 <dbgusdn2048@naver.com>
2023-04-26 17:49:40 +0900

    2023-04-26

--- src/test/programmers/S176963.java
+++ src/test/programmers/S176963.java
@@ -1,19 +1,24 @@
 package test.programmers;
 
+import java.util.*;
+
 public class S176963 {
 	public static void main(String[] args) {
 		String[] name = {"may", "kein", "kain", "radi"};
 		int[] yearning = {5, 10, 1, 3};
-		String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+		String[][] photo = {{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+		
 		int answer[] = new int[photo.length];
-		System.out.println(photo.length);
-		int sum = 0;
-		for (int i = 0; i < yearning.length; i++) {
-			sum += yearning[i];
-		}
 		
 		for (int i = 0; i < photo.length; i++) {
-			
+			for (int j = 0; j < photo[i].length; j++) {
+				for (int n = 0; n < name.length; n++) {
+					if (name[n].equals(photo[i][j])) {
+						answer[i] += yearning[n];
+					}
+				}
+			}
 		}
+		System.out.println(Arrays.toString(answer));
 	}
 }
---
+++ src/test/programmers/S178871.java
@@ -0,0 +1,14 @@
+package test.programmers;
+
+public class S178871 {
+	public static void main(String[] args) {
+		String[] players = {"mumu", "soe", "poe", "kai", "mine"};
+		String[] callings = {"kai", "kai", "mine", "mine"};
+		
+		String[] answer = new String[players.length];
+		
+		for (int i = 0; i < players.length; i++) {
+			asdasdasdasd
+		}
+	}
+}
